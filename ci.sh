1124a7e699a03d9f30396caf794a8d75a650703c
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-02 19:14:54 +0530

    storing last commit as a variable now

--- ci.sh
+++ ci.sh
@@ -1,17 +1,16 @@
 #! /usr/bin/env sh
 ​
-userName=armanaaquib
-repoName=ci-cd
+userName=step-batch-7
+repoName=softwareSamurai-settlers
 ​
-touch lastCommitSha.txt
+lastCommitSha=""
 
 while :
 do
-  curl -H "Authorization: token OAUTH-TOKEN" https://api.github.com/repos/$userName/$repoName/commits/master > latestCommit.txt
+  curl -H "Authorization: token ebb34770dd011e4d3e8a2c6155ccf5ea3cbf35ea" https://api.github.com/repos/$userName/$repoName/commits/master > latestCommit.txt
 
   latestCommitSha=$(cat latestCommit.txt | grep sha | head -1 | cut -d\" -f4)
   latestCommitterName=$(cat latestCommit.txt | grep login | head -1 | cut -d\" -f4)
-  lastCommitSha=$(cat lastCommitSha.txt)
 ​
   if [ "$latestCommitSha" != "$lastCommitSha" ]
   then
@@ -21,14 +20,14 @@ do
     npm test
     if [ $? -ne 0 ]
     then
-      cd ../ci-cd
+      cd ../CI-Tool
       echo "tests are failing"
       sed -e 's/__status-color__/red/g' -e s/__committer-name__/$latestCommitterName/g -e s/__commit-sha__/$latestCommitSha/g -e 's/__status__/failing/g' template.html > ci.html
     else
-      cd ../ci-cd
+      cd ../CI-Tool
       echo "tests are passing"
       sed -e 's/__status-color__/green/g' -e s/__committer-name__/$latestCommitterName/g -e s/__commit-sha__/$latestCommitSha/g -e 's/__status__/passing/g' template.html > ci.html
-      echo $latestCommitSha > lastCommitSha.txt
+      lastCommitSha=$(echo $latestCommitSha)
     fi
   else
     echo "no latest commit"
