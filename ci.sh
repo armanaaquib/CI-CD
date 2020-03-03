eb502d8588f58f9e0b1546aa3ee1f83e6d50926f
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-03 11:04:58 +0530

    now it will work for any repo

--- ci.sh
+++ ci.sh
@@ -1,36 +1,49 @@
 #! /usr/bin/env sh
 ​
-userName=user-name
-repoName=repo-name
+echo 'Enter User-Name:'
+read userName
+echo 'Enter Repo-Name:'
+read repoName
 ​
-lastCommitSha=""
+git clone https://github.com/$userName/$repoName.git
+lastCommitSha=none
 
+updateDisplayPage(){
+  sed -e "s/__repo-name__/$repoName/g" -e "s/__status-color__/$1/g" -e "s/__committer-name__/$2/g" -e "s/__commit-sha__/$3/g" -e "s/__status__/$4/g" template.html > ci.html
+}
+
+updateDisplayPage yellow "loading..." "loading..." "staring ci"
+open ci.html
+
+oauthToken=$(cat OAUTH-TOKEN.txt)
 while :
 do
-  curl -H "Authorization: token OAUTH-TOKEN" https://api.github.com/repos/$userName/$repoName/commits/master > latestCommit.txt
+  curl -H "Authorization: token $oauthToken" https://api.github.com/repos/$userName/$repoName/commits/master > latestCommit.txt
 
   latestCommitSha=$(cat latestCommit.txt | grep sha | head -1 | cut -d\" -f4)
   latestCommitterName=$(cat latestCommit.txt | grep login | head -1 | cut -d\" -f4)
+
   if [ "$latestCommitSha" != "$lastCommitSha" ]
   then
-    cd ../$repoName
-    git pull
+    git clone https://github.com/$userName/$repoName.git
+    cd $repoName
     npm install
     npm test
     if [ $? -ne 0 ]
     then
-      cd ../CI-Tool
+      cd ..
       echo "tests are failing"
-      sed -e 's/__status-color__/red/g' -e s/__committer-name__/$latestCommitterName/g -e s/__commit-sha__/$latestCommitSha/g -e 's/__status__/failing/g' template.html > ci.html
+      updateDisplayPage red $latestCommitterName $latestCommitSha "tests are failing"
     else
-      cd ../CI-Tool
+      cd ..
       echo "tests are passing"
-      sed -e 's/__status-color__/green/g' -e s/__committer-name__/$latestCommitterName/g -e s/__commit-sha__/$latestCommitSha/g -e 's/__status__/passing/g' template.html > ci.html
+      updateDisplayPage green $latestCommitterName $latestCommitSha "tests are passing"
       lastCommitSha=$(echo $latestCommitSha)
     fi
+    rm -rf $repoName
   else
     echo "no latest commit"
   fi
   rm latestCommit.txt
-  sleep 1s
+  sleep 30s
 done
